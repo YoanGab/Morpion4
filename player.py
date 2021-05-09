60ca79753e570a1de00d181607b8cb62b92b2b69
Yoan <yoan.gabison@gmail.com>
2021-05-09 21:07:58 +0200

    Fix player.py errors

--- player.py
+++ player.py
@@ -1,3 +1,10 @@
+class Player():
+    def __init__(self, letter):
+        self.letter = letter
+
+    def get_move(self, game):
+        pass
+
 class HumanPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
@@ -5,15 +12,16 @@ class HumanPlayer(Player):
     def get_move(self, game):
         valid_square = False
         val = None
-        column = input(f"{self.letter}'s turn. Column move (1-{game.size}): ")
-        line = input(f"{self.letter}'s turn. Line move (1-{game.size}): ")
-        try:
-            column = int(column) - 1
-            line = int(line) - 1
-            val = [column, line]
-            if val not in game.available_moves():
-                raise ValueError
-            valid_square = True
-        except ValueError:
-            print('Invalid square. Try again.')
+        while not valid_square:
+            column = input(f"{self.letter}'s turn. Column move (1-{game.size}): ")
+            line = input(f"{self.letter}'s turn. Line move (1-{game.size}): ")
+            try:
+                column = int(column) - 1
+                line = int(line) - 1
+                val = [column, line]
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print('Invalid square. Try again.')
         return val
